43916763b79c3ff54c7841833085264d69f2b985
Illarion <pro100illarion@gmail.com>
2023-10-06 19:36:37 +0300

    Deleted unnecessary code, renamed variables and properties, fixed problems with `OnPropertyChanged`

--- Model/DbUser.cs
+++ Model/DbUser.cs
@@ -1,115 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
-using Xk7.Helper;
 using Xk7.Helper.Converts;
 
 namespace Xk7.Model
 {
     internal class DbUser : IDbUser, ICloneable, INotifyPropertyChanged
     {
-        private uint idUserRole;
-        private string login;
-        private byte[] hashPassword;
-        private string firstName;
-        private string secondName;
-        private DateTime dateBirthday;
-        private bool isBlocked;
+        private uint _idUserRole;
+        private string _login;
+        private byte[] _hashPassword;
+        private string _firstName;
+        private string _secondName;
+        private DateTime _dateBirthday;
+        private bool _isBlocked;
         public uint IdUserRole
         {
             get
             {
-                return idUserRole;
+                return _idUserRole;
             }
             set
             {
-                idUserRole = value;
-                OnPropertyChanged("IdTimetable");
+                _idUserRole = value;
+                OnPropertyChanged("IdUserRole");
             }
         }
-
-        public string? Login
+        public string Login
         {
             get
             {
-                return login;
+                return _login;
             }
             set
             {
-                login = value;
-                OnPropertyChanged("EmployeeLogin");
+                _login = value;
+                OnPropertyChanged("Login");
             }
         }
         public byte[] HashPassword
         {
             get
             {
-                return hashPassword;
+                return _hashPassword;
             }
             set
             {
-                hashPassword = value;
-                OnPropertyChanged("SlotDate");
+                _hashPassword = value;
+                OnPropertyChanged("HashPassword");
             }
         }
         public string FirstName
         {
             get
             {
-                return firstName;
+                return _firstName;
             }
             set
             {
-                firstName = value;
-                OnPropertyChanged("SlotTime");
+                _firstName = value;
+                OnPropertyChanged("FirstName");
             }
         }
-
         public string SecondName
         {
             get
             {
-                return secondName;
+                return _secondName;
             }
             set
             {
-                secondName = value;
-                OnPropertyChanged("SlotTime");
+                _secondName = value;
+                OnPropertyChanged("SecondName");
             }
         }
         public DateTime DateBirthday
         {
             get
             {
-                return dateBirthday;
+                return _dateBirthday;
             }
             set
             {
-                dateBirthday = value;
-                OnPropertyChanged("SlotTime");
+                _dateBirthday = value;
+                OnPropertyChanged("DateBirthday");
             }
         }
-
         public bool IsBlocked
         {
             get
             {
-                return isBlocked;
+                return _isBlocked;
             }
             set
             {
-                isBlocked = value;
-                OnPropertyChanged("SlotTime");
+                _isBlocked = value;
+                OnPropertyChanged("IsBlocked");
             }
         }
-
-        public DbUser() {}
-        public DbUser(uint idUserRole, string login, byte[] hashPassword, string firstName, string secondName,
-            DateTime dateBirthday, bool isBlocked)
+        public DbUser() : this(0, string.Empty, new byte[0], string.Empty, string.Empty, DateTime.FromFileTimeUtc(0), false) {}
+        public DbUser(uint idUserRole, string login, byte[] hashPassword, string firstName, string secondName, DateTime dateBirthday, bool isBlocked)
         {
             IdUserRole = idUserRole;
             Login = login;
@@ -119,46 +110,32 @@ namespace Xk7.Model
             DateBirthday = dateBirthday;
             IsBlocked = isBlocked;
         }
-
         public DbUser(User user)
         {
             IdUserRole = (uint)user.IdUserRole;
             Login = user.Login;
             HashPassword = Encoding.UTF8.GetBytes(user.Password.Value);
-            FirstName = firstName;
-            SecondName = secondName;
+            FirstName = user.FirstName;
+            SecondName = user.SecondName;
             DateBirthday = new DateTime(user.DateBirthday.Year, user.DateBirthday.Month, user.DateBirthday.Day, 0, 0, 0);
-            IsBlocked = isBlocked;
+            IsBlocked = user.IsBanned;
         }
-
+        public bool AllFieldsFilled()
+        {
+            return Login != string.Empty && HashPassword != null && HashPassword.Length != 0 && FirstName != string.Empty
+                && SecondName != string.Empty;
+        }
         public override string ToString()
         {
             return
                 $"[ IdUserRole: {IdUserRole.ToString()}, Login: {Login}, HashPassword: {Converts.ConvertByteArrayToString(HashPassword)}, FirstName: {FirstName}, " +
                 $"SecondName: {SecondName}, DateBirthday: {DateBirthday}, IsBlocked: {IsBlocked}]";
         }
-
-        public static bool operator ==(DbUser user1, DbUser user2)
-        {
-            return user1.IdUserRole == user2.IdUserRole && user1.IsBlocked == user2.IsBlocked && user1.Login == user2.Login
-                && user1.HashPassword == user2.HashPassword && user1.FirstName == user2.FirstName && user1.SecondName == user2.SecondName
-                && user1.DateBirthday == user2.DateBirthday;
-        }
-
-        public static bool operator !=(DbUser user1, DbUser user2)
-        {
-            return !(user1.IdUserRole == user2.IdUserRole && user1.IsBlocked == user2.IsBlocked && user1.Login == user2.Login
-                && user1.HashPassword == user2.HashPassword && user1.FirstName == user2.FirstName && user1.SecondName == user2.SecondName
-                && user1.DateBirthday == user2.DateBirthday);
-        }
-
         public object Clone()
         {
             return new DbUser(IdUserRole, Login, HashPassword, FirstName, SecondName, DateBirthday, IsBlocked);
         }
-
         public event PropertyChangedEventHandler? PropertyChanged;
-
         private void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             if (PropertyChanged != null)
