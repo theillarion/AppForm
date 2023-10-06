58e6dbc161532d3ab6e0aa1abe1ad85eeac27b7c
Illarion <pro100illarion@gmail.com>
2023-10-06 19:52:01 +0300

    Added a new interface `IFileService`. Necessary to perform operations with the file server.

---
+++ Services/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Xk7.Helper.Enums;
+
+namespace Xk7.Services
+{
+    internal interface IFileService
+    {
+        Task<LoadFileResult> UploadFileAsync(string srcFilePath, string destFilePath, bool isOverride = false);
+        Task<LoadFileResult> DownloadFileAsync(string srcFilePath, string destFilePath, bool isOverride = false);
+        bool ExistsPath(string path);
+        Task<bool> ExistsPathAsync(string path);
+        Task<RemoveResult> RemoveFileAsync(string filePath);
+        bool CreateDirectory(string path);
+        Task<bool> CreateDirectoryAsync(string path);
+        Task<RemoveResult> RemoveDirectoryAsync(string path);
+    };
+}
