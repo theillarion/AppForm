021f51806ca57403f73165465db5b9328963dec1
Illarion <pro100illarion@gmail.com>
2023-10-14 20:45:35 +0300

    Added a new `UserControl` component for presenting tests

---
+++ Components/TestCard.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Xk7.Components
+{
+    public partial class TestCard : UserControl
+    {
+        public TestCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
