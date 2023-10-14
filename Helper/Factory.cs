9f470df8af0790ec444c67e1b75bcc447f131c3e
Illarion <pro100illarion@gmail.com>
2023-10-14 20:57:30 +0300

    Added type conversion when converting a database record to an object

--- Helper/Factory.cs
+++ Helper/Factory.cs
@@ -18,6 +18,19 @@ namespace Xk7.Helper
             return potentialDescendant.IsSubclassOf(potentialBase)
                    || potentialDescendant == potentialBase;
         }
+        private static object ChangeType(object value, Type conversion)
+        {
+            var t = conversion;
+
+            if (t.IsGenericType && t.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+            {
+                if (value == null)
+                    return null;
+                t = Nullable.GetUnderlyingType(t);
+            }
+
+            return Convert.ChangeType(value, t);
+        }
         private static void SetItemFromRow<T>(T? item, DataRow? row) where T : new()
         {
             if (item == null || row == null)
@@ -28,7 +41,7 @@ namespace Xk7.Helper
 
                 if (p != null && row[c] != DBNull.Value)
                 {
-                    p.SetValue(item, row[c], null);
+                    p.SetValue(item, ChangeType(row[c], p.PropertyType), null);
                 }
             }
         }
