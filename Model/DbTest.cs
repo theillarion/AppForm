4eb9e2711efdbe119314d0cc1581a4b8e0086bdc
Illarion <pro100illarion@gmail.com>
2023-10-14 21:00:22 +0300

    Added model of `Test` table in the database

--- Model/DbTest.cs
+++ Model/DbTest.cs
@@ -1,138 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xk7.Helper.Enums;
 
 namespace Xk7.Model
 {
-    internal class DbTest
+	internal class DbTest : IDbTest, IComparable
     {
-        protected uint _id;
-        protected string _tittle;
-        protected string _description;
-        protected DateTime _utcDateTimeCreated;
-        protected string _whoCreated;
-        protected bool _hasImage;
-        public uint Id
+		public int Id { get; set; } = 0;
+		public string Tittle { get; set; } = string.Empty;
+		public string? Description { get; set; } = null;
+		public DateTime? _utcDateTimeCreated = null;
+		public DateTime UtcDateTimeCreated
+		{
+			get
+			{
+				if (_utcDateTimeCreated == null)
+					_utcDateTimeCreated = DateTime.UtcNow;
+                return _utcDateTimeCreated ?? DateTime.UtcNow;
+			}
+			set
+			{
+				_utcDateTimeCreated = value;
+            }
+		}
+		public string WhoCreated { get; set; } = string.Empty;
+		public bool HasImage { get; set; } = false;
+		public DbTest() { }
+        public DbTest(int id, string tittle, string? description, DateTime utcDateTimeCreated, string whoCreated, bool hasImage)
+		{
+			Id = id;
+			Tittle = tittle;
+			Description = description;
+			UtcDateTimeCreated = utcDateTimeCreated;
+			WhoCreated = whoCreated;
+			HasImage = hasImage;
+		}
+		public bool AllFieldsFilled() => Tittle != string.Empty && _utcDateTimeCreated != null && WhoCreated != string.Empty;
+        public int CompareTo(object? other)
         {
-            get
-            {
-                return _id;
-            }
-            set
-            {
-                _id = value;
-                OnPropertyChanged("Id");
-            }
-        }
-        public string Tittle
-        {
-            get
-            {
-                return _tittle;
-            }
-            set
-            {
-                _tittle = value;
-                OnPropertyChanged("Tittle");
-            }
-        }
-        public string Description
-        {
-            get
-            {
-                return _description;
-            }
-            set
-            {
-                _description = value;
-                OnPropertyChanged("Description");
-            }
-        }
-        public DateTime UtcDateTimeCreated
-        {
-            get
-            {
-                return _utcDateTimeCreated;
-            }
-            set
-            {
-                _utcDateTimeCreated = value;
-                OnPropertyChanged("UtcDateTimeCreated");
-            }
-        }
-        public string WhoCreated
-        {
-            get
-            {
-                return _whoCreated;
-            }
-            set
-            {
-                _whoCreated = value;
-                OnPropertyChanged("WhoCreated");
-            }
-        }
-        public bool HasImage
-        {
-            get
-            {
-                return _hasImage;
-            }
-            set
-            {
-                _hasImage = value;
-                OnPropertyChanged("HasImage");
-            }
-        }
-        internal DbTest() { }
-        internal DbTest(uint id, string tittle, string description, DateTime utcDateTimeCreated, string whoCreated, bool hasImage)
-        {
-            _id = id;
-            _tittle = tittle;
-            _description = description;
-            _utcDateTimeCreated = utcDateTimeCreated;
-            _whoCreated = whoCreated;
-            _hasImage = hasImage;
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string prop = "")
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+			if (other != null && other is DbTest dbtest)
+				return Id.CompareTo(dbtest.Id);
+			return 1;
         }
     }
-    internal class DbTestWithImage : DbTest
+	internal class DbTestWithImage : DbTest, IDbTestWithImage
     {
-        protected string _pathImage;
-        public string PathImage
-        {
-            get
-            {
-                return _pathImage;
-            }
-            set
-            {
-                _pathImage = value;
-                OnPropertyChanged("PathImage");
-            }
-        }
-        internal DbTestWithImage() { }
-        internal DbTestWithImage(uint id, string tittle, string description, DateTime utcDateTimeCreated, string pathImage)
-        {
-            _id = id;
-            _tittle = tittle;
-            _description = description;
-            _utcDateTimeCreated = utcDateTimeCreated;
-            _hasImage = true;
-            _pathImage = pathImage;
-        }
+		public string Path { get; set; } = string.Empty;
+        public string LocalPath { get; set; } = string.Empty;
+		public DbTestWithImage() { }
+        public DbTestWithImage(int id, string tittle, string? description, DateTime utcDateTimeCreated, string whoCreated, bool hasImage, string pathImage, string localPathImage)
+			: base(id, tittle, description, utcDateTimeCreated, whoCreated, hasImage)
+		{
+			Path = pathImage;
+			LocalPath = localPathImage;
+		}
+		new public bool AllFieldsFilled() => base.AllFieldsFilled() && Path != string.Empty;
     }
 }
---
+++ Model/IDbTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Xk7.Model
+{
+    public interface IDbTest : IDbBase
+    {
+        public static string DbFormatDateTime = "yyyy-MM-dd hh:mm:ss";
+        int Id { get; set; }
+        string Tittle { get; set; }
+        string? Description { get; set; }
+        DateTime UtcDateTimeCreated { get; set; }
+        string WhoCreated { get; set; }
+        bool HasImage { get; set; }
+    }
+    public interface IDbTestWithImage : IDbTest
+    {
+        string Path { get; set; }
+        string LocalPath { get; set; }
+    }
+}
