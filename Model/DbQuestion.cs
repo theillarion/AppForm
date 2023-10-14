d8b00815ebffd1ef7f3d15ac7e6891dc1488c2f2
Illarion <pro100illarion@gmail.com>
2023-10-14 21:01:01 +0300

    Added model of `Question` table in the database

---
+++ Model/DbQuestion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Xk7.Model.DbQuestion;
+
+namespace Xk7.Model
+{
+	internal class DbQuestion : IDbQuestion, IComparable
+	{
+		public int Id { get; set; } = 0;
+		public int IdTest { get; set; } = 0;
+		public uint IdType { get; set; } = 1;
+		public int OrdinalNumber { get; set; } = 0;
+		public string Text { get; set; } = string.Empty;
+		public bool HasImage { get; set; } = false;
+		public DbQuestion() { }
+		public DbQuestion(int id, int idTest, uint idType, int ordinalNumber, string text, bool hasImage)
+		{
+			Id = id;
+			IdTest = idTest;
+			IdType = idType;
+			OrdinalNumber = ordinalNumber;
+			Text = text;
+			HasImage = hasImage;
+		}
+		public DbQuestion(int idTest, string text)
+		{
+			IdTest = idTest;
+			Text = text;
+		}
+		public bool AllFieldsFilled() => IdTest > 0 & Text != string.Empty;
+		public int CompareTo(object? other)
+		{
+			if (other != null && other is DbQuestion dbQuestion)
+			{
+				int result = IdTest.CompareTo(dbQuestion.IdTest);
+				int ordinalNumber = OrdinalNumber.CompareTo(dbQuestion.OrdinalNumber);
+				return (result != 0) ? result : (ordinalNumber != 0) ? ordinalNumber : Id.CompareTo(dbQuestion.Id);
+			}
+			return 1;	
+		}
+	}
+	internal class DbQuestionWithImage : DbQuestion, IDbQuestionWithImage
+	{
+		public string Path { get; set; } = string.Empty;
+		public string LocalPath { get; set; } = string.Empty;
+		public DbQuestionWithImage() { }
+		public DbQuestionWithImage(int id, int idTest, uint idType, int ordinalNumber, string text, bool hasImage, string path, string localPath) : base(id, idTest, idType, ordinalNumber, text, hasImage)
+		{
+			Path = path;
+			LocalPath = localPath;
+		}
+		new public bool AllFieldsFilled() => base.AllFieldsFilled() && Path != string.Empty;
+	}
+}
---
+++ Model/IDbQuestion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xk7.Model
+{
+    internal interface IDbQuestion : IDbBase
+    {
+        int Id { get; set; }
+        int IdTest { get; set; }
+        uint IdType { get; set; }
+        int OrdinalNumber { get; set; }
+        string Text { get; set; }
+        bool HasImage { get; set; }
+    }
+    internal interface IDbQuestionWithImage : IDbQuestion
+    {
+        string Path { get; set; }
+        string LocalPath { get; set; }
+    }
+}
