afd6c2d1ead834962bdd42b409d615da2068a332
Illarion <pro100illarion@gmail.com>
2023-10-14 20:49:35 +0300

    The `Add?Result` enumerations have been moved to a single file

---
+++ Helper/Enums/AddResults.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xk7.Helper.Enums
+{
+    public enum CommonAddResult
+    {
+        Success,
+        NotExistsUser,
+        Unknown,
+    }
+    public enum AddUserResult
+    {
+        Success,
+        UserExists,
+        Unknown,
+    }
+    internal enum AddTestResult
+    {
+        Success,
+        TestExists,
+        Unknown
+    }
+    internal enum AddQuestionResult
+    {
+        Success,
+        QuestionExists,
+        TestNotExists,
+        Unknown
+    }
+    internal enum AddImageResult
+    {
+        Success,
+        IdNotExists,
+        ImageExists,
+        Unknown
+    }
+    internal enum AddAnswerResult
+    {
+        Success,
+        QuestionNotExists,
+        AnswerExists,
+        Unknown
+    }
+}
