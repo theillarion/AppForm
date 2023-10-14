f1c8d4b2d3173e85b500014f7eb9e742394bf2bb
Illarion <pro100illarion@gmail.com>
2023-10-14 21:06:53 +0300

    The implementation of the hashed value has been changed. Added a new field.

--- Model/HashedValue.cs
+++ Model/HashedValue.cs
@@ -1,11 +1,12 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using Xk7.Helper.Converts;
 
 namespace Xk7.Model
 {
     public class HashedValue
     {
-        private string _originalValue;
+        private string _originalValue = string.Empty;
         public string OriginalValue
         {
             get => _originalValue;
@@ -13,18 +14,14 @@ namespace Xk7.Model
             {
                 _originalValue = value;
                 Value = Converts.ConvertStringToHeshString(value);
+                ByteArray = Encoding.UTF8.GetBytes(value);
             }
         }
-        public string Value { get; private set; }
-
-        private HashedValue() { }
+        public string Value { get; private set; } = string.Empty;
+        public byte[] ByteArray { get; private set; } = Array.Empty<byte>();
         public HashedValue(string originalValue)
         {
             OriginalValue = originalValue;
-        }
-        public override string ToString()
-        {
-            return Value;
         }
     }
 }
