12224e73ce419c80a8064556b3ea4561fd0bc406
Illarion <pro100illarion@gmail.com>
2023-10-14 21:01:56 +0300

    Added a static class to get the name of database tables

---
+++ Model/DbNameTables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xk7.Model
+{
+    public enum TestTables
+    {
+        Test,
+        Question,
+        ImageTest,
+        ImageQuestion,
+        Answer
+    }
+    internal static class TestNameTables
+    {
+
+        public static string[] Values = { "Test", "Question", "TestImage", "QuestionImage", "SimpleAnswer" };
+        public static string GetValue(TestTables testTables) => Values[(int)testTables];
+        public static string Test { get => Values[(int)TestTables.Test]; }
+        public static string Question { get => Values[(int)TestTables.Question]; }
+        public static string ImageTest { get => Values[(int)TestTables.ImageTest]; }
+        public static string ImageQuestion { get => Values[(int)TestTables.ImageQuestion]; }
+        public static string Answer { get => Values[(int)TestTables.Answer]; }
+    }
+}
