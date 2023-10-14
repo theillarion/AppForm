fa41191e832cb94fff52333e9001e2c49995e0ed
Illarion <pro100illarion@gmail.com>
2023-10-14 21:23:57 +0300

    Added `ViewModel` for working with tests

---
+++ ViewModel/TestViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xk7.ViewModel
+{
+    public class TestViewModel : INotifyPropertyChanged
+    {
+        private string _title;
+        public string Title {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+        private string _description;
+        public string? Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+        private string _path;
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                _path = value;
+                OnPropertyChanged(nameof(Path));
+            }
+        }
+        public TestViewModel(string title, string? description, string path)
+        {
+            Title = title;
+            Description = description;
+            Path = path;
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
