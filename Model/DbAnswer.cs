692b33e54f7858446aa7403d8117e4bb58a86469
Illarion <pro100illarion@gmail.com>
2023-10-14 21:00:44 +0300

    Added model of `Answer` table in the database

---
+++ Model/DbAnswer.cs
@@ -0,0 +1,44 @@
+﻿using Org.BouncyCastle.Crypto;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xk7.Model
+{
+	internal class DbSimpleAnswer : IDbAnswer, IComparable
+	{
+		public int Id { get; set; } = 0;
+		public int IdQuestion { get; set; } = 0;
+		private string? _text = null;
+		public string Text
+		{
+			get => _text ?? string.Empty;
+			set
+			{
+				_text = value;
+			}
+		}
+		public bool IsCorrect { get; set; } = false;
+		public DbSimpleAnswer() { }
+		public DbSimpleAnswer(int id, int idQuestion, string text, bool isCorrect)
+		{
+			Id = id;
+			IdQuestion = idQuestion;
+			Text = text;
+			IsCorrect = isCorrect;
+		}
+		public bool AllFieldsFilled() => IdQuestion != 0 && _text != null;
+		public int CompareTo(object? other)
+		{
+			if (other != null && other is DbSimpleAnswer dbSimpleAnswer)
+			{
+                int result = IdQuestion.CompareTo(dbSimpleAnswer.IdQuestion);
+                return (result != 0) ? result : Id.CompareTo(dbSimpleAnswer.Id);
+            }
+			return 1;
+		}
+	}
+}
---
+++ Model/IDbAnswer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xk7.Model
+{
+    internal interface IDbAnswer : IDbBase
+    {
+        int Id { get; set; }
+        int IdQuestion { get; set; }
+        string Text { get; set; }
+        bool IsCorrect { get; set; }
+    }
+}
