6212a7bacc543256e650a7a4ac40fb164c65c80a
Illarion <pro100illarion@gmail.com>
2023-10-14 20:55:54 +0300

    Added support for inserting a `Nullable object into a table

--- Helper/Extensions/DbCommandExtension.cs
+++ Helper/Extensions/DbCommandExtension.cs
@@ -10,11 +10,11 @@ namespace Xk7.Helper.Extensions
 {
     public static class DbCommandExtension
     {
-        public static void AddParameterWithValue(this DbCommand command, string parameterName, object parameterValue)
+        public static void AddParameterWithValue(this DbCommand command, string parameterName, object? parameterValue)
         {
             var parameter = command.CreateParameter();
             parameter.ParameterName = parameterName;
-            parameter.Value = parameterValue;
+            parameter.Value = parameterValue ?? DBNull.Value;
             command.Parameters.Add(parameter);
         }
     }
